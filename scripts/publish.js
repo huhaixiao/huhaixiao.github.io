36e9491b84230444b74edea6113b5b19adb6e74d
Haixiao Hu <haixhu@icloud.com>
2023-03-29 22:36:43 +0800

    update

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,30 +1,38 @@
 const process = require("process");
 const path = require("path");
-const fs = require("fs");
-const childProcess = require('child_process');
+const childProcess = require("child_process");
 
-const clientDir = path.resolve(process.cwd(), './apps/client/dist');
-console.log('line: 5 ',process.cwd());
+const clientDir = path.resolve(process.cwd(), "./apps/client/dist");
 process.chdir(clientDir);
-console.log(process.cwd());
-console.log('line: 7 ',process.cwd());
-childProcess.exec('git init', (error, stdout, stderr) => {
-  console.log(stdout);
-  childProcess.exec('git commit -am "update"', (error, stdout, stderr) => {
-    console.log('...git add . ... start')
-    console.log(error)
-    console.log(stdout)
-    console.log(stderr)
-    console.log('...git add . ... end')
-    childProcess.exec('git log', (error, stdout, stderr) => {
-      console.log(stdout)
-      childProcess.exec('git remote add origin git@github.com:huhaixiao/huhaixiao.github.io.git', (error, stdout, stderr) => {
-        childProcess.exec('git checkout -b main', (error, stdout, stderr) => {
-            childProcess.exec('git push origin HEAD -f', (error, stdout, stderr) => {
-            console.log(stdout)
-          })
-        })
-      })
-    })
-  })
-})
+
+async function runCLI (cli) {
+  return new Promise((resolve) => {
+    childProcess.exec(cli, resolve);
+  });
+}
+
+async function publishToGithub () {
+  try {
+    await runCLI("rm -rf .git/");
+    await runCLI("git init");
+    await runCLI("git add .");
+    await runCLI("git commit -m \"setup\"");
+    await runCLI("git remote add origin git@github.com:huhaixiao/huhaixiao.github.io.git");
+    await runCLI("git checkout -b main");
+    await runCLI("git push origin HEAD -f");
+    console.log("publish to github success");
+  } catch (e) {
+    console.log("publish to github failed");
+  }
+}
+
+async function publishToGitee () {
+
+}
+
+async function publish () {
+  await publishToGithub();
+  await publishToGitee();
+}
+
+publish();

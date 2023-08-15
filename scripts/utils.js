2d922d4a043a2099a60005a0e24223d3dad1c47c
Haixiao Hu <haixhu@icloud.com>
2023-08-15 22:24:19 +0800

    setup

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,16 +3,15 @@ const childProcess = require("child_process");
 function runCLI (cli) {
   return new Promise((resolve) => {
     console.log(`running ${cli}`);
-    const instance = childProcess.exec(cli);
-    resolve(instance);
+    childProcess.exec(cli, {}, resolve);
   });
 }
 
-function open (url) {
+function open(url) {
   childProcess.exec(`start ${url}`);
 }
 
 module.exports = {
   runCLI,
-  open
+  open,
 };
